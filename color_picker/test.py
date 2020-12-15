f343f592100f7ef02c50ea0029b30fe51cfeadbb
Eike <eike.boergeling@gmx.de>
2020-12-15 16:41:10 +0100

    heftige verschlimmbesserung

--- color_picker/test.py
+++ color_picker/test.py
@@ -1,6 +1,5 @@
 import cv2
 import mido
-import numpy as np
 import rtmidi as midi
 
 config = {}
@@ -23,30 +22,10 @@ except Exception:
     )
     exit(1)
 
-
 midiOut = midi.MidiOut()
 midiOutput = mido.open_output(config["MidiDevice"])
 
 threshold = int(config["Threshold"])
-
-# define range of blue color in HSV
-lower_blue = np.array([110, 50, 0])
-upper_blue = np.array([130, 255, 255])
-# define range of green color in HSV
-lower_green = np.array([60, 50, 0])
-upper_green = np.array([80, 255, 255])
-# define range of red color in HSV
-lower_red = np.array([160, 100, 0])
-upper_red = np.array([200, 255, 255])
-# define range of yellow color in HSV
-lower_yellow = np.array([20, 100, 0])
-upper_yellow = np.array([40, 255, 255])
-# define range of turquoise color in HSV
-lower_turquoise = np.array([80, 100, 0])
-upper_turquoise = np.array([100, 255, 255])
-# define range of purple color in HSV
-lower_purple = np.array([140, 100, 0])
-upper_purple = np.array([160, 255, 255])
 
 count = 0
 cv2.namedWindow("all", 0)
@@ -62,63 +41,114 @@ def playNote(note):
     midiOutput.send(message)  # MIDI Senden
 
 
+colorDict = {
+    "red": {
+        "hsv": [0, 100, 255],
+        "mask": 0,
+        "count": 0,
+        "cal": 0,
+    },
+    "green": {
+        "hsv": [120, 100, 0],
+        "mask": 0,
+        "count": 0,
+        "cal": 0,
+    },
+    "blue": {
+        "hsv": [240, 100, 0],
+        "mask": 0,
+        "count": 0,
+        "cal": 0,
+    },
+    "cyan": {
+        "hsv": [196, 76, 0],
+        "mask": 0,
+        "count": 0,
+        "cal": 0,
+    },
+    "magenta": {
+        "hsv": [300, 100, 0],
+        "mask": 0,
+        "count": 0,
+        "cal": 0,
+    },
+    "yellow": {
+        "hsv": [53, 57, 0],
+        "mask": 0,
+        "count": 0,
+        "cal": 0,
+    },
+}
+
+colors = ["blue", "green", "red", "yellow", "turquoise", "purple"]
+calibrateValues = {
+    "blue": 0, "green": 0, "red": 0,
+    "yellow": 0, "turquoise": 0, "purple": 0,
+}
+colorValues = {}
+colorMasks = {}
+
+
+def calibrateNow():
+    print("ISCH KALIBRIERE JEZZ!!")
+    for k, v in colorDict.items():
+        colorDict[k]["cal"] = colorDict[k]["count"]
+
+
 while cap.isOpened():
     ret, frame = cap.read()
+    currentFrameHsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+    frameH, frameS, frameV = cv2.split(currentFrameHsv)
 
-    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+    for key, value in colorDict.items():
+        currentColor = key
+        hue = value["hsv"][0]
+        sat = value["hsv"][1]
 
-    # Threshold the HSV image to get only blue colors
-    mask_blue = cv2.inRange(hsv, lower_blue, upper_blue)
-    # Threshold the HSV image to get only gree colors
-    mask_green = cv2.inRange(hsv, lower_green, upper_green)
-    # Threshold the HSV image to get only red colors
-    mask_red = cv2.inRange(hsv, lower_red, upper_red)
-    # Threshold the HSV image to get only blue colors
-    mask_yellow = cv2.inRange(hsv, lower_yellow, upper_yellow)
-    # Threshold the HSV image to get only gree colors
-    mask_turquoise = cv2.inRange(hsv, lower_turquoise, upper_turquoise)
-    # Threshold the HSV image to get only red colors
-    mask_purple = cv2.inRange(hsv, lower_purple, upper_purple)
+        th = 10
+        maskHue = cv2.inRange(frameH, hue - th, hue + th)
+        maskSat = cv2.inRange(frameS, sat - th, sat + th)
+        maskHueSat = maskHue ** maskSat
+        colorDict[currentColor]["mask"] = maskHueSat
+        colorDict[currentColor]["count"] = \
+            colorDict[currentColor]["mask"].sum()
 
-    masks = [
-        mask_blue, mask_green, mask_red,
-        mask_yellow, mask_purple, mask_turquoise,
-    ]
+        cv2.imshow(currentColor, maskHue)
 
-    numBlue = mask_blue.sum()
-    numGreen = mask_green.sum()
-    numRed = mask_red.sum()
-    numYellow = mask_yellow.sum()
-    numTurquoise = mask_turquoise.sum()
-    numPurple = mask_purple.sum()
+        if (
+            colorDict[currentColor]["count"] -
+            colorDict[currentColor]["cal"]
+        ) > threshold:
+            print(currentColor)
 
-    if count == 100:
-        print("-----------------")
-        print("Blue: {}".format(numBlue))
-        print("Green: {}".format(numGreen))
-        print("Red: {}".format(numRed))
-        print("Yellow: {}".format(numYellow))
-        print("Purple: {}".format(numPurple))
-        print("turquoise: {}".format(numTurquoise))
-        print("-----------------")
-        count = 0
+    #
+    # if count == 50:
+    #     print("-------------------")
+    #     for c in colors:
+    #         print(c, ":", colorValues[c])
+    #
+    #     count = 0
+    #     print("-------------------")
 
-    numArray = [numBlue, numGreen, numRed, numYellow, numPurple, numTurquoise]
-    numMax = np.max(numArray)
-    if numMax > threshold:
-        for i in range(len(numArray)):
-            if numArray[i] == numMax:
-                frame = cv2.bitwise_and(frame, frame, mask=masks[i])
-                playNote(i)
-
-    else:
-        playNote(6)
+    # frame = cv2.bitwise_and(frame, frame, mask=v["mask"])
+    # numMax = np.max(numArray)
+    # if numMax > threshold:
+    #     for i in range(len(numArray)):
+    #         if numArray[i] == numMax:
+    #             frame = cv2.bitwise_and(frame, frame, mask=masks[i])
+    #             #playNote(i)
+    #
+    # else:
+    #     playNote(6)
 
     count += 1
     cv2.imshow("all", frame)
 
-    if cv2.waitKey(25) != -1:
+    key = cv2.waitKey(25)
+    if key == 27:
         break
+    elif key != -1:
+        calibrateNow()
 
 cap.release()
 cv2.destroyAllWindows()
