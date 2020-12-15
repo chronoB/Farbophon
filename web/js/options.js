3c34427ac6c18812c19528fb95ea32f833046d68
chronoB <finn.o.bayer@gmail.com>
2020-12-15 21:50:13 +0100

    added all config parameters to options screen

--- web/js/options.js
+++ web/js/options.js
@@ -1,18 +1,24 @@
-document.addEventListener("DOMContentLoaded", init)
+document.addEventListener("DOMContentLoaded", showConfig)
 
-function init() {
-    showCurAPIKey()
-}
 
-function showCurAPIKey() {
+function showConfig() {
     fetch("/config")
         .then(response => response.text())
         .then(data => {
             let lines = data.split("\r\n")
             lines.forEach(line => {
                 let parts = line.split("=")
+                if (parts[0] === "CamDevice") {
+                    document.querySelector("#curCamDevice").innerText = parts[1]
+                }
+                if (parts[0] === "Threshold") {
+                    document.querySelector("#curThreshold").innerText = parts[1]
+                }
                 if (parts[0] === "API-Key") {
                     document.querySelector("#curApiKey").innerText = parts[1]
+                }
+                if (parts[0] === "MidiDevice") {
+                    document.querySelector("#curMidiController").innerText = parts[1]
                 }
             })
         })
