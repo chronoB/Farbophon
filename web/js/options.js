18e40f9178a11c2a22380b9afc10e6023fab21e4
chronoB <finn.o.bayer@gmail.com>
2020-12-15 19:45:31 +0100

    added config, updated options

--- web/js/options.js
+++ web/js/options.js
@@ -1,7 +1,20 @@
-document.querySelector("#setAPIButton").addEventListener("click", setAPIKey)
+document.addEventListener("DOMContentLoaded", init)
 
+function init() {
+    showCurAPIKey()
+}
 
-function setAPIKey() {
-    let key = document.querySelector("#apiInput").value
-    console.log(key)
+function showCurAPIKey() {
+    fetch("/config")
+        .then(response => response.text())
+        .then(data => {
+            let lines = data.split("\r\n")
+            lines.forEach(line => {
+                let parts = line.split("=")
+                if (parts[0] === "API-Key") {
+                    document.querySelector("#curApiKey").innerText = parts[1]
+                }
+            })
+        })
+
 }
