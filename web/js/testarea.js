0f98c0fa5c5a1a95d123a7b82998ae6e1a5a0e6b
chronoB-Laptop <finn.o.bayer@gmail.com>
2020-12-22 20:19:01 +0100

    pre-commit

--- web/js/testarea.js
+++ web/js/testarea.js
@@ -1,109 +1,121 @@
 document.addEventListener("DOMContentLoaded", initTestArea)
-let c;
-let ctx;
-let cH;
-let cW;
-let bgColor = "#000000";
-let animations = [];
-let circles = [];   
-let colors = ["#ff0000","#00ff00","#0000ff","#ffff00","#ff00ff","#00ffff"];
+let c
+let ctx
+let cH
+let cW
+let animations = []
+let colors = ["#ff0000", "#00ff00", "#0000ff", "#ffff00", "#ff00ff", "#00ffff"]
 
-function initTestArea(){
+function initTestArea() {
     c = document.getElementById("c")
-    ctx = c.getContext("2d");
+    ctx = c.getContext("2d")
 
-    resizeCanvas();
-    window.addEventListener("resize", resizeCanvas);
+    resizeCanvas()
+    window.addEventListener("resize", resizeCanvas)
 }
 
-function evalMidiInput(note){
-  drawParticles(note)
+function evalMidiInput(note) {
+    drawParticles(note)
 }
 
 function removeAnimation(animation) {
-  let index = animations.indexOf(animation);
-  if (index > -1) animations.splice(index, 1);
+    let index = animations.indexOf(animation)
+    if (index > -1) animations.splice(index, 1)
 }
-
 
 function drawParticles(colorIdx) {
     let currentColor = colors[colorIdx]
-    let rippleSize = Math.min(200, (cW * .4));
-    
-    let particles = [];
+    let minRippleSize = 200
+    let rippleScale = 0.4
+    let rippleSize = Math.min(minRippleSize, cW * rippleScale)
+
+    let particles = []
     let x = anime.random(0, cW)
     let y = anime.random(0, cH)
-    for (let i=0; i<32; i++) {
-      let particle = new Circle({
-        x: x, //TODO: Random
-        y: y,//TODO: Random
-        fill: currentColor,
-        r: anime.random(24, 48)
-      })
-      particles.push(particle);
+    let numParticles = 32
+    let minRadiusParticle = 24
+    let maxRadiusParticle = 48
+
+    for (let i = 0; i < numParticles; i++) {
+        let particle = new Circle({
+            x: x, //TODO: Random
+            y: y, //TODO: Random
+            fill: currentColor,
+            r: anime.random(minRadiusParticle, maxRadiusParticle),
+        })
+        particles.push(particle)
     }
+    let minAnimationTime = 1000
+    let maxAnimationTime = 1000
     let particlesAnimation = anime({
-      targets: particles,
-      x: function(particle){
-        return particle.x + anime.random(rippleSize, -rippleSize);
-      },
-      y: function(particle){
-        return particle.y + anime.random(rippleSize * 1.15, -rippleSize * 1.15);
-      },
-      r: 0,
-      easing: "easeOutExpo",
-      duration: anime.random(1000,1300),
-      complete: removeAnimation
-    });
-    animations.push(particlesAnimation);
+        targets: particles,
+        x: function (particle) {
+            return particle.x + anime.random(rippleSize, -rippleSize)
+        },
+        y: function (particle) {
+            let rippleHeightScale = 1.15
+            return (
+                particle.y +
+                anime.random(
+                    rippleSize * rippleHeightScale,
+                    -rippleSize * rippleHeightScale
+                )
+            )
+        },
+        r: 0,
+        easing: "easeOutExpo",
+        duration: anime.random(minAnimationTime, maxAnimationTime),
+        complete: removeAnimation,
+    })
+    animations.push(particlesAnimation)
 }
 
-function extend(a, b){
-  for(let key in b) {
-    if(b.hasOwnProperty(key)) {
-      a[key] = b[key];
+function extend(a, b) {
+    for (let key in b) {
+        if (b.hasOwnProperty(key)) {
+            a[key] = b[key]
+        }
     }
-  }
-  return a;
+    return a
 }
 
-let Circle = function(opts) {
-  extend(this, opts);
+let Circle = function (opts) {
+    extend(this, opts)
 }
 
-Circle.prototype.draw = function() {
-  ctx.globalAlpha = this.opacity || 1;
-  ctx.beginPath();
-  ctx.arc(this.x, this.y, this.r, 0, 2 * Math.PI, false);
-  if (this.stroke) {
-    ctx.strokeStyle = this.stroke.color;
-    ctx.lineWidth = this.stroke.width;
-    ctx.stroke();
-  }
-  if (this.fill) {
-    ctx.fillStyle = this.fill;
-    ctx.fill();
-  }
-  ctx.closePath();
-  ctx.globalAlpha = 1;
+Circle.prototype.draw = function () {
+    ctx.globalAlpha = this.opacity || 1
+    ctx.beginPath()
+    ctx.arc(this.x, this.y, this.r, 0, 2 * Math.PI, false)
+    if (this.stroke) {
+        ctx.strokeStyle = this.stroke.color
+        ctx.lineWidth = this.stroke.width
+        ctx.stroke()
+    }
+    if (this.fill) {
+        ctx.fillStyle = this.fill
+        ctx.fill()
+    }
+    ctx.closePath()
+    ctx.globalAlpha = 1
 }
 
 let animate = anime({
-  duration: Infinity,
-  update: function() {
-    ctx.clearRect(0, 0, cW, cH);
-    animations.forEach(function(anim) {
-      anim.animatables.forEach(function(animatable) {
-        animatable.target.draw();
-      });
-    });
-  }
-});
+    duration: Infinity,
+    update: function () {
+        ctx.clearRect(0, 0, cW, cH)
+        animations.forEach(function (anim) {
+            anim.animatables.forEach(function (animatable) {
+                animatable.target.draw()
+            })
+        })
+    },
+})
 
-let resizeCanvas = function() {
-  cW = window.innerWidth;
-  cH = window.innerHeight;
-  c.width = cW * devicePixelRatio;
-  c.height = cH * devicePixelRatio;
-  ctx.scale(devicePixelRatio, devicePixelRatio);
-};
+let resizeCanvas = function () {
+    cW = window.innerWidth
+    cH = window.innerHeight
+    c.width = cW * devicePixelRatio
+    c.height = cH * devicePixelRatio
+    ctx.scale(devicePixelRatio, devicePixelRatio)
+}
