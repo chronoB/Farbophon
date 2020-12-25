d435e86e9703017e3d366cde759d58245fcaff70
chronoB-Laptop <finn.o.bayer@gmail.com>
2020-12-25 18:45:07 +0100

    added login/registration

--- web/js/communication.js
+++ web/js/communication.js
@@ -1,9 +1,50 @@
 document.addEventListener("DOMContentLoaded", initialize)
 
 function initialize() {
-    if (!sessionStorage.getItem("API-Key")) {
+    if (!sessionStorage.getItem("Server-URI")) {
         getConfiguration()
     }
+}
+
+function register(user, password) {
+    payload = {
+        name: user,
+        password: password,
+    }
+    let options = {
+        method: "post",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(payload),
+    }
+    return fetch(
+        sessionStorage.getItem("Server-URI") + "/farbophon/register",
+        options
+    ).then((request) => {
+        return request.json().then((data) => {
+            return data
+        })
+    })
+}
+
+function login(user, password) {
+    let options = {
+        method: "post",
+        headers: {
+            "Content-Type": "application/json",
+            authorization: "Basic " + btoa(user + ":" + password),
+        },
+    }
+    return fetch(
+        sessionStorage.getItem("Server-URI") + "/farbophon/login",
+        options
+    ).then((request) => {
+        return request.json().then((data) => {
+            sessionStorage.setItem("Server-Token", data.token)
+            return data
+        })
+    })
 }
 
 function sendHighscore(user, score) {
@@ -15,7 +56,7 @@ function sendHighscore(user, score) {
     let options = {
         method: "post",
         headers: {
-            "x-access-tokens": sessionStorage.getItem("API-Key"),
+            "x-access-tokens": sessionStorage.getItem("Server-Token"),
             "Content-Type": "application/json",
         },
         body: JSON.stringify(payload),
--- web/js/communication_test.js
+++ web/js/communication_test.js
@@ -2,12 +2,41 @@ document.addEventListener("DOMContentLoaded", initialize)
 
 function initialize() {
     document
+        .querySelector("#registerButton")
+        .addEventListener("click", (event) => registerUser(event))
+    document
+        .querySelector("#loginButton")
+        .addEventListener("click", (event) => loginUser(event))
+    document
         .querySelector("#inputButton")
         .addEventListener("click", (event) => triggerHighscore(event))
     document
         .querySelector("#showButton")
         .addEventListener("click", (event) => showHighscore(event))
 }
+
+function registerUser(event) {
+    event.preventDefault()
+
+    let user = document.querySelector("#registerNameInput").value
+    let pw = document.querySelector("#registerPasswordInput").value
+    register(user, pw).then((data) => {
+        document.querySelector("#outputText").innerHTML = JSON.stringify(data)
+    })
+}
+
+function loginUser(event) {
+    event.preventDefault()
+
+    let user = document.querySelector("#loginNameInput").value
+    let pw = document.querySelector("#loginPasswordInput").value
+
+    login(user, pw).then((data) => {
+        document.querySelector("#outputText").innerHTML =
+            JSON.stringify(data) + "\ntoken saved in sessionstorage."
+    })
+}
+
 function triggerHighscore(event) {
     event.preventDefault()
 
@@ -15,17 +44,13 @@ function triggerHighscore(event) {
     let score = document.querySelector("#scoreInput").value
 
     sendHighscore(user, score).then((data) => {
-        document.querySelector("#highscoreText").innerHTML = JSON.stringify(
-            data
-        )
+        document.querySelector("#outputText").innerHTML = JSON.stringify(data)
     })
 }
 
 function showHighscore(event) {
     event.preventDefault()
     getHighscore().then((data) => {
-        document.querySelector("#highscoreText").innerHTML = JSON.stringify(
-            data
-        )
+        document.querySelector("#outputText").innerHTML = JSON.stringify(data)
     })
 }
