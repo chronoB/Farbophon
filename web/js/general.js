9f6e1a4f7bdf8ad115e2b30195a229dea8b4c25e
chronoB <finn.o.bayer@gmail.com>
2021-01-03 16:21:10 +0100

    check if config is loaded to avoid reload, restructure code

--- web/js/general.js
+++ web/js/general.js
@@ -1,6 +1,7 @@
 document.addEventListener("DOMContentLoaded", init)
 
 function init() {
+    checkIfConfigIsLoaded()
     let navButtons = document.querySelectorAll(".nav-button")
     navButtons.forEach((btn) => {
         btn.addEventListener("click", (event) => initAfterLoader(event))
@@ -24,4 +25,37 @@ function initAfterLoader(event) {
 
 function redirectToPage(target) {
     window.location = target.dataset.target
+}
+
+//Configuration
+
+function checkIfConfigIsLoaded() {
+    if (sessionStorage.getItem("isConfigLoaded") === null) {
+        getConfiguration()
+    }
+}
+
+function getConfiguration() {
+    let identifierIndex = 0
+    let valueIndex = 1
+
+    fetch("/config")
+        .then((response) => response.text())
+        .then((data) => {
+            //split every line into a new object.
+            //every line of the config file is a new config object
+            let lines = data.split("\r\n")
+            lines.forEach((line) => {
+                //split the configuration in two parts
+                //identifier=value
+                let parts = line.split("=")
+                //save the configuration in the sessionStorage
+                sessionStorage.setItem(
+                    parts[identifierIndex],
+                    parts[valueIndex]
+                )
+            })
+            sessionStorage.setItem("isConfigLoaded", 1)
+            const DEV_MODE = parseInt(sessionStorage.getItem("DevMode"))
+        })
 }
