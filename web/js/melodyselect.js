833e9585448970360d1db8a1b2236f5094ee9e90
chronoB <finn.o.bayer@gmail.com>
2021-01-03 22:26:47 +0100

    added melodyfile selection screen to singleplayer

---
+++ web/js/melodyselect.js
@@ -0,0 +1,23 @@
+document.addEventListener("DOMContentLoaded", addSelectOptions)
+
+function addSelectOptions() {
+    fetch("/web/melodyfiles/filelist.txt")
+        .then((response) => response.text())
+        .then((data) => {
+            let lines = data.split("\r\n")
+            lines = data.split("\n")
+            lines.forEach((line) => {
+                if (line === "") return
+                let option = document.createElement("option")
+                option.setAttribute("id", line)
+                option.innerText = line
+                document.getElementById("melodyselect").appendChild(option)
+            })
+        })
+}
+
+function selectMelody() {
+    let selectEl = document.getElementById("melodyselect")
+    if (selectEl.value === "") return
+    sessionStorage.setItem("melodyFile", selectEl.value)
+}
